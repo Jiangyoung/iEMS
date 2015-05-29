8d9736065847234e27bf59fc73603e7128283f69
Jiangyoung <jiangyoungzh@163.com>
2015-05-29 21:53:17 +0800

    入口文件修改

--- www/index.php
+++ www/index.php
@@ -4,10 +4,17 @@
  *
  */
 
+if(!stripos($_SERVER['REQUEST_URI'],'index.php')){
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location:index.php');
+}
+
 define('GAPP_BASEDIR', dirname(__DIR__).'/App');
 define('GAPP_APPNAME', 'Index');
 define('GAPP_TPLDIR', GAPP_BASEDIR.'/../templates');
 define('GAPP_ROOT_PATH',GAPP_BASEDIR.'/../www');
+
+
 define('GAPP_ROOT_URL',dirname($_SERVER['REQUEST_URI']));
 
 require GAPP_BASEDIR . '/' . 'Common/Autoload/Autoload.class.php';
